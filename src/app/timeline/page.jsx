0a804be6f0f5e01dbedb25dc87f8af857cdcaebe
KanizSanjila <kanizsm13@gmail.com>
2026-04-15 11:19:56 +0600

    Timeline data added

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import { FaHandshake } from "react-icons/fa";
 export const metadata = {
   title: "TimeLine Page | Keen-keeper",
 };
 
 const TimeLinePage = () => {
-    return (
-        <div>
-           <h1>timeline page</h1> 
+  return (
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">Timeline</h1>
+      <div className="mb-4">
+        <select className="select select-bordered w-60">
+          <option disabled selected>
+            Filter timeline
+          </option>
+          <option>All</option>
+          <option>Meetups</option>
+          <option>Events</option>
+        </select>
+      </div>
+
+      <div className="bg-white rounded-lg shadow-sm p-4 flex items-center gap-4">
+        
+        <div className="text-yellow-500 text-xl">
+          <FaHandshake />
         </div>
-    );
+
+        <div>
+          <p className="text-gray-800">
+            <span className="font-semibold">Meetup</span> with Tom Baker
+          </p>
+          <p className="text-sm text-gray-500">March 29, 2026</p>
+        </div>
+
+      </div>
+    </div>
+  );
 };
 
 export default TimeLinePage;
